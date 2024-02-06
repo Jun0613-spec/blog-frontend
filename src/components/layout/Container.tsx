79071f925d0ff8bcee6790a71172832362842911
Jun0613-spec <kumo5110@gmail.com>
2024-02-06 11:18:12 +0000

    fixed

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Container = () => {
-  const { pathname } = useLocation();
-
   return (
     <>
       <Header />
