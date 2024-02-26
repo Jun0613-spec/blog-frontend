5e30259859e81be7c6d8795a751cc12ab7309423
Jun0613-spec <kumo5110@gmail.com>
2024-02-26 09:03:34 +0000

    deleted App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import "./App.css";
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
